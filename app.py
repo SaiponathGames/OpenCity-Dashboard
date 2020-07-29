976b1855e678afc2a5b7bfb7d492497234c3e178
sairam4123 <opencity@info.github.com>
2020-07-29 11:21:48 +0530

    Update 0.1.2 by Sairam4123
    Add loggedin.html
    
    Add route for loggedin.html

--- app.py
+++ app.py
@@ -8,5 +8,10 @@ def hello_world():
     return render_template("html/index.html")
 
 
+@app.route('/loggedin')
+def logged_in():
+    return render_template("html/loggedin.html")
+
+
 if __name__ == '__main__':
     app.run()
